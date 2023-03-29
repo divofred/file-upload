4460c97e620c939185aaa417f5de9f7c70eabf05
Fredrick Emmanuel <54718010+divofred@users.noreply.github.com>
2023-03-29 14:57:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/files");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storage })
+
+app.use(express.static("public/Files"));
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.redirect('/upload')
+});
+app.post('/upload', upload.single('photo'), (req,res)=>{
+  fs.readdir("public/files", function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.render("upload", { data });
+  });
+})
+app.get('/upload', (req,res)=>{
+  fs.readdir("public/files", function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.render("upload", { data });
+  });
+})
+app.listen(PORT, () => {
+  console.log("app started at port ", PORT);
+});
